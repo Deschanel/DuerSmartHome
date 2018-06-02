e6ba41b225354cdc9433a80440a51049ea87ea69
xiaozhuo1314 <chenzhuo1116@qq.com>
2018-06-02 15:48:37 +0800

    增加设备

--- gateDuerOS.php
+++ gateDuerOS.php
@@ -35,29 +35,88 @@ switch($obj->header->namespace){
 				),
 				array(
 					"actions"  =>  array("turnOn", "turnOff"),
-					"applianceTypes"  => array("CURTAIN"),
+					"applianceTypes"  => array("HUMIDIFIER"),
 					"additionalApplianceDetails"  =>  array(),
 					"applianceId"  =>  "switch.pump",
-					"friendlyDescription" =>  "SwitchDeviceId",
-					"friendlyName"  =>  "卧室的窗帘",
+					"friendlyDescription" =>  "浇水",
+					"friendlyName"  =>  "植物浇水器",
 					"isReachable"  =>  true,
-					"manufacturerName"  =>  "Nodemcu",
-					"modelName"  =>  "fancyCurtain",
+					"manufacturerName"  =>  "broadlink",
+					"modelName"  =>  "植物浇水器",
 					"version"  =>  "1.0"
 				),
 				array(
 					"actions"  =>  array("turnOn", "turnOff"),
 					"applianceTypes"  => array("LIGHT"),
 					"additionalApplianceDetails"  =>  array(),
-					"applianceId"  =>  "LlightTwo",
-					"friendlyDescription" =>  "LightDeviceId",
-					"friendlyName"  =>  "厨房的灯",
+					"applianceId"  =>  "switch.light",
+					"friendlyDescription" =>  "补光",
+					"friendlyName"  =>  "植物补光器",
 					"isReachable"  =>  true,
-					"manufacturerName"  =>  "Nodemcu",
-					"modelName"  =>  "fancyLight",
+					"manufacturerName"  =>  "broadlink",
+					"modelName"  =>  "植物补光器",
 					"version"  =>  "1.0"
-				)
-			)
+				),
+				array(
+					"actions"  =>  array("turnOn", "turnOff"),
+					"applianceTypes"  => array("HEATER"),
+					"additionalApplianceDetails"  =>  array(),
+					"applianceId"  =>  "switch.temperature",
+					"friendlyDescription" =>  "温度",
+					"friendlyName"  =>  "植物调温器",
+					"isReachable"  =>  true,
+					"manufacturerName"  =>  "broadlink",
+					"modelName"  =>  "植物调温器",
+					"version"  =>  "1.0"
+				),
+				array(
+					"actions"  =>  array("getTemperatureReading"),
+					"applianceTypes"  => array("AIR_MONITOR"),
+					"additionalApplianceDetails"  =>  array(),
+					"applianceId"  =>  "sensor.plant_temperature",
+					"friendlyDescription" =>  "温度",
+					"friendlyName"  =>  "花花草草温度",
+					"isReachable"  =>  true,
+					"manufacturerName"  =>  "miflora",
+					"modelName"  =>  "花花草草温度",
+					"version"  =>  "1.0"
+			 ),
+			 array(
+				 "actions"  =>  array("getHumidity"),
+				 "applianceTypes"  => array("AIR_MONITOR"),
+				 "additionalApplianceDetails"  =>  array(),
+				 "applianceId"  =>  "sensor.plant_moisture",
+				 "friendlyDescription" =>  "湿度",
+				 "friendlyName"  =>  "花花草草湿度",
+				 "isReachable"  =>  true,
+				 "manufacturerName"  =>  "miflora",
+				 "modelName"  =>  "花花草草湿度",
+				 "version"  =>  "1.0"
+			),
+			array(
+				"actions"  =>  array("getWaterQuality"),
+				"applianceTypes"  => array("AIR_MONITOR"),
+				"additionalApplianceDetails"  =>  array(),
+				"applianceId"  =>  "sensor.plant_conductivity",
+				"friendlyDescription" =>  "电导率",
+				"friendlyName"  =>  "花花草草电导率",
+				"isReachable"  =>  true,
+				"manufacturerName"  =>  "miflora",
+				"modelName"  =>  "花花草草电导率",
+				"version"  =>  "1.0"
+		  ),
+			array(
+				"actions"  =>  array("getRunningStatus"),
+				"applianceTypes"  => array("AIR_MONITOR"),
+				"additionalApplianceDetails"  =>  array(),
+				"applianceId"  =>  "sensor.plant_battery",
+				"friendlyDescription" =>  "电量",
+				"friendlyName"  =>  "花花草草电量",
+				"isReachable"  =>  true,
+				"manufacturerName"  =>  "miflora",
+				"modelName"  =>  "花花草草电量",
+				"version"  =>  "1.0"
+		  )
 		);
 		//$payload = json_encode($payload);
 		$resultStr = json_encode(array("header" => $header, "payload" => $payload));
